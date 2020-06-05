c37b1ff9f4e05091dd1e6d806d3b2a3da9d5911c
Ben Yorke <bendyorke@Bens-MacBook-Air.local>
2020-06-05 17:25:18 +0200

    Get the gql injector up and running

--- src/main/phrasing/events.cljs
+++ src/main/phrasing/events.cljs
@@ -3,19 +3,51 @@
             [ajax.core :as ajax]
             [kee-frame.core :as kee]))
 
-(defn gq
-  ([query] (gq query {}))
-  ([query vars]
-   (->> {:query query :variables vars}
-        (clj->js)
-        (.stringify js/JSON))))
+(defn gql-operation
+  "Get the operation (:op) from the gql constructor.
+  Will be one of :query, :mutation, or :subscription"
+  [gql]
+  (-> (get gql :op :query)
+      name))
 
-(defn gql->http-xhrio [{gql :gql :as context}]
+(defn gql-definitions
+  "Get the definitions (:defs) from the gql constructor.
+  Key value pair of {:name \"Type\"}"
+  [gql]
+  (->> (get gql :defs {})
+       (reduce-kv #(str %1 "$" (name %2) ":" %3 ",") "")))
+
+(defn gql-query
+  "Get the query (:query) from the gql constructor.
+  Strip leading and trailing curly braces, as they are included
+  as part of the query string template."
+  [gql]
+  (-> (get gql :query "")
+      (clojure.string/replace #"^\{?(.*)\}?$" "$1")))
+
+(defn construct-gql-data
+  "Convert a gql constructor to a json string ready to be sent to
+  the server. Will return {:query query :variables varibles} where
+  :query is a fully templated query string and :variables is the :vars
+  key in the gql constructor"
+  [gql]
+  (let [query (str (gql-operation gql) "(" (gql-definitions gql) "){" (gql-query gql) "}")]
+    (->> {:query query :variables (gql :vars)}
+         (clj->js)
+         (.stringify js/JSON))))
+
+(defn gql->http-xhrio
+  "Takes the gql constructor from the context and assocs the appropriate
+  http-xhrio data needed to submit it as a request"
+  [{gql :gql :as context}]
   (assoc context :http-xhrio {:method          :post
-                              :uri             "https://localhost:4000/api"
+                              :uri             "http://localhost:4000/api"
                               :headers         {"Content-Type" "application/json"}
+                              :body            (construct-gql-data gql)
                               :format          (ajax/json-request-format)
-                              :response-format (ajax/json-response-format {:keywords? true})}))
+                              :response-format (ajax/json-response-format {:keywords? true})
+                              :on-success      [::sign-in-success]
+                              :on-failure      [::sign-in-failure]}))
 
 (def gql
   (re-frame.core/->interceptor
@@ -26,17 +58,9 @@
 (reg-event-fx ::sign-in
   [gql]
   (fn [cofx [_ input]]
-    {:http-xhrio {:method          :post
-                  :uri             "http://localhost:4000/api"
-                  :body            (gq "mutation($input:SessionInput!){signIn(input:$input){token}}" {:input input})
-                  :headers         {"Content-Type" "application/json"}
-                  :format          (ajax/json-request-format)
-                  :response-format (ajax/json-response-format {:keywords? true})
-                  :on-success      [::sign-in-success]
-                  :on-failure      [::sign-in-failure]}
-     :gql {:op    :mutation
+    {:gql {:op    :mutation
            :defs  {:input "SessionInput!"}
-           :args  {:input input}
+           :vars  {:input input}
            :query "signIn(input:$input) { token }"}}))
 
 (reg-event-db ::sign-in-failure
@@ -53,4 +77,8 @@
     (println event)
     db))
 
-
+; (println
+;   (construct-gql-data {:op    :mutation
+;                        :defs  {:input "SessionInput!"}
+;                        :vars  {:input {:email "email" :password "password"}}
+;                        :query "signIn(input:$input) { token }"}))
